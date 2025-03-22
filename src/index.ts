cabd39fe059a51351bbee9d4732b181eceefbc35
svnrnns <haruueel@gmail.com>
2025-03-22 10:20:49 +0100

    fix: types exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ import { useEventListener } from "./useEventListener";
 const eventEmit = useEventEmit;
 
 export { useEventEmit, useEventListener, eventEmit };
+
+// TypeScript
+import { EventMap, EventCallback } from "./types";
+export type { EventMap, EventCallback };
